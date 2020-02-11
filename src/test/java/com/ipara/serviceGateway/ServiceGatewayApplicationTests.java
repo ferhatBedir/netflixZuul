132127bce8f79892e20305a30527d8d6de0ebcb3
ferhat.bedir@hotmail.com <Fb1231992>
2020-02-11 20:43:18 +0300

    push

---
+++ src/test/java/com/ipara/serviceGateway/ServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipara.serviceGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
